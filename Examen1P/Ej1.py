666526059af27ba139aea21799c40f9061dba4ae
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-02-07 19:48:22 -0600

    Update Ej1.py

--- Examen1P/Ej1.py
+++ Examen1P/Ej1.py
@@ -1,7 +1,7 @@
 palabra = input("Introduce una palabra: ")
 
-letras = [letra for letra in palabra]
+letras = [(i+1, letra)for i, letra in enumerate(palabra)]
 print("Las letras que tiene tu palabra son: ")
 
 for letra in letras:
-    print(letra)
+    print("Letra",letra)
