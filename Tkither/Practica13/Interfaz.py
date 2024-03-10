9bbee731c5fe2baeb2df339c9e3a37d11207cad3
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-03-09 23:31:10 -0600

    Finalizacion practica 13

--- Tkither/Practica13/Interfaz.py
+++ Tkither/Practica13/Interfaz.py
@@ -1,24 +1,59 @@
 import tkinter as tk
-from tkinter import Tk, messagebox, Button
+from tkinter import messagebox
+from clase import *
 
-def mostrarMensajes():
-    print(messagebox.askquestion(message="Deseas incluir Mayusculas en tu contrasena?", title="Mayusculas"))
+    #Se crea una clase con los widgets para la creacion de la intefaz usando Tkinter 
+class winGeneradorContrasena:
+    def __init__(self, ventana):
+        print (" Se ha creado un objeto de la clase winGeneradorContrasena")
+        self.ventana = ventana
+        ventana.title("Generador de Contraseñas")
 
-Ventana=Tk()
-Ventana.title("Generador de Contraseñas")
-Ventana.geometry("600x400")
+        self.intLongitud_label = tk.Label(ventana, text="Longitud de la Contraseña:")
+        self.intLongitud_label.pack()
 
-longitudEtiqueta = tk.Label(Ventana, text="Longitud:")
-longitudEtiqueta.pack()
+        self.intLongitud_entry = tk.Entry(ventana)
+        self.intLongitud_entry.pack()
 
-logitud = tk.Entry(Ventana)
-logitud.insert(0, "8")
-logitud.pack()
+        self.generate_button = tk.Button(ventana, text="Generar Contraseña", command=self.cmdGeneraPassword)
+        self.generate_button.pack()
 
-botonGenerar=tk.Button(logitud,text="Generar Contraseña" command=mostrarMensajes)
-botonGenerar.pack()
+        self.passEntry = tk.Entry(ventana, text="")
+        self.passEntry.pack()
 
-contra_entry = tk.Entry(Ventana, show="*")
-contra_entry.pack()
 
-Ventana.mainloop()
+    def cmdGeneraPassword(self):
+        objGenerador = clase()
+        respuesta = messagebox.askquestion("Pregunta", "¿Quieres incluir mayusculas?")
+        if respuesta == "yes":
+            blnIncluyeMayusculas = True
+        else:
+            blnIncluyeMayusculas = False            
+        respuesta = messagebox.askquestion("Pregunta", "¿Quieres incluir caracteres especiales?")
+        if respuesta == "yes":
+            blnIncluyeCaracteresEspeciales = True
+        else:
+            blnIncluyeCaracteresEspeciales = False            
+
+        try:
+            #aqui se optiene la longitud designada por el usuario en donde si la contrasena es menor a 8 el programa arroja un mensaje de error 
+            intLongitud = int(self.intLongitud_entry.get())
+            if intLongitud <= 7:
+                raise ValueError("La longitud debe ser un número positivo mayor que ocho")
+
+            #se crea un objeto de tipo password con los atributos seleccionados por el usuario en donde si selecciona mayusculas y caracter especial 
+            password = objGenerador.GeneraContrasena(intLongitud, blnIncluyeMayusculas, blnIncluyeCaracteresEspeciales)
+
+            # Aqui se muestra la contra generada en un cuadro de texto para que pueda ser copiada por el usuario 
+            self.passEntry.delete(0, tk.END)
+            self.passEntry.insert(0, password)
+            strFortaleza = objGenerador.VerificaFortalezaContrasena(password)
+            messagebox.showinfo ("Fortaleza del password generado", strFortaleza)
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
+
+
+ventana = tk.Tk()
+ventana.geometry("350x150")
+app = winGeneradorContrasena(ventana)
+ventana.mainloop()
--- Tkither/Practica13/clase.py
+++ Tkither/Practica13/clase.py
@@ -1,15 +1,28 @@
 import random
 import string
+import re
 
-class generadorContrasena():
-
-    def generadorcontra(length=8, include_uppercase=False, include_special_chars=False):
-        chars = string.ascii_lowercase
+class clase:
+    def GeneraContrasena(self, length, include_uppercase=False, include_special=False):
+        # se determina si la contrasena incluye mayusculas y caracter especial
+        characters = string.ascii_lowercase
         if include_uppercase:
-            chars += string.ascii_uppercase
-        if include_special_chars:
-            chars += string.punctuation
+            characters += string.ascii_uppercase
+        if include_special:
+            characters += string.punctuation
 
-        password = ''.join(random.choice(chars) for _ in range(length))
-        return password
-
+        # retorna el valor de la contra generada 
+        return ''.join(random.choice(characters) for _ in range(length))
+    
+    #Metodo para la verificacion de la contrasena en donde evalua la longitud, si incluye mayusculas o algun caracter especial
+    def VerificaFortalezaContrasena(self, contraseña):
+        if len(contraseña) < 12:
+            return "Se recomiendan al menos 12 caracteres de longitud."
+        
+        if not re.search(r'[!@#$%^&*()_+{}[\]:;<>,.?/~]', contraseña):
+            return "Se recomienda al menos algun caracter de tipo especial"
+        
+        if not re.search(r'[A-Z]', contraseña):
+            return "Se recomienda al menos el uso de una letra mayuscula"
+        
+        return "Tu contrasena es lo duficientemente fuerte"
